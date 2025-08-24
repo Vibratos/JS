ed7a69ac06af98cf1af3b9c8a25bc105a69c46ad
Vit <vitlito@gmail.com>
2025-08-24 21:35:29 +0300

    random position

---
+++ bark_function.js
@@ -0,0 +1,13 @@
+function bark(name, weight) {
+if (weight > 20) {
+console.log(name + " says WOOF WOOF");
+} else {
+console.log(name + " says woof woof");
+}
+}
+bark("juno", 20);
+bark("scottie", -1);
+bark("dino", 0, 0);
+bark("fido", "20");
+bark("lady", false);
+bark("bruno", 21);
